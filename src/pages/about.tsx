f480ff83fe3b9715d368c8b2a98717e7483fe6da
ben-dh3 <benjaminduffield1997@gmail.com>
2024-06-27 12:23:59 +0100

    about section added

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,22 +1,22 @@
 
 export default function About() {
     return(
-        <div className="mt-20 text-center space-y-10" id="about">
-            <div className="text-8xl font-medium">SELF EXPRESSION FOR YOUR</div>
-            <div className="text-7xl font-AzeretMonoItalic">EYE-NEEDS</div>
-
-            <div className="text-3xl m-40 space-y-10">
-                <div className="text-colour">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+        <div className="mt-20 text-center space-y-40" id="about">
+            <div>
+                <div className="text-8xl font-medium">SELF EXPRESSION FOR YOUR</div>
+                <div className="text-8xl font-AzeretMonoItalic">EYE-NEEDS</div>
+                <div className="text-4xl mt-10 mx-80 text-colour">
+                    Ben Duffield-Harding, an artist from Stratford-Upon-Avon and based in London, England, is a creative with a fascination with many art mediums: painting, generative with code, pixel.
+                </div>
+            </div>
+            
+            <div className="text-3xl mx-80 space-y-10">
+                
+                <div className="">
+                    Bold honesty characterises Ben's work, displaying colours and patterns in a way that captures the energy of a subject.
                 </div>
                 <div>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </div>
-                <div>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    A drive for adventure and curiosity of human nature. His far-reaching travels and varied career pursuits characterise a creative mind with a thirst for learning and discovery.
                 </div>
             </div>  
         </div>
